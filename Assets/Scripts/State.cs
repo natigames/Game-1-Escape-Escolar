35d7b108a6f8fabb0faa8f6274919d3860f4eb5c
Juan Carlos Cardoso <papo@nati.games>
2020-01-08 21:35:28 -0600

    Initial Commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+public class State : ScriptableObject
+{
+    [TextArea(10,14)] [SerializeField] string StoryText;
+    [SerializeField] State[] NextStates;
+
+    public string getStateStory()
+    {
+        return StoryText;
+    }
+
+    public State[] getNextStates()
+    {
+        return NextStates;           
+    }
+}
---
+++ Assets/Scripts/TextAdventure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAdventure : MonoBehaviour
+{
+
+    [SerializeField] Text TextComponent;
+    [SerializeField] State StartingState;
+
+    State State;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        State = StartingState;
+        TextComponent.text = State.getStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+
+        var nextStates = State.getNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 +index))
+            {
+                State = nextStates[index];
+            }
+        }
+
+        TextComponent.text = State.getStateStory();
+    }
+}
